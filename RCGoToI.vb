de3050f8271631876d5f1160d52f85d8b479e3c5
Nicolas Woo <nlhwoo03@gmail.com>
2023-09-02 06:04:38 -0600

    tweaked functionality for future-proofing

--- RCGoToI.vb
+++ RCGoToI.vb
@@ -2,6 +2,7 @@ Sub RC_GoTo_Invoicing()
     Dim captainCol As Integer
     Dim boatCol As Integer
     Dim currentRow As Integer
+    Dim fiscalYear As Integer
     Dim fullName As String
     Dim nameArr() As String
     Dim lastName As String
@@ -12,11 +13,14 @@ Sub RC_GoTo_Invoicing()
     Dim basePath As String
     Dim fullPath As String
     
+    fiscalYear = FiscalYearOf(ActiveSheet.Cells(ActiveCell.Row, FindInRow(1, "Start (noon)")).Value)
+    Debug.Print
+    
     If ActiveSheet.name = "Charters" Then
         captainCol = FindInRow(1, "Captain")
         boatCol = FindInRow(1, "Boat")
         currentRow = ActiveCell.Row
-        basePath = Environ("USERPROFILE") & "\Documents\SLTC\9 SLTC - Documents\Seabbatical\Fleet\" 'this should change based on computer/file setup (the first three directories)
+        basePath = ThisWorkbook.Path 'tweaked to work wherever, as long as the current file setup stays the same
     
         fullName = ActiveSheet.Cells(currentRow, captainCol).Value
         nameArr = Split(fullName, " ")
@@ -26,7 +30,7 @@ Sub RC_GoTo_Invoicing()
         End If
     
         boatName = ActiveSheet.Cells(currentRow, boatCol).Value
-        fullPath = basePath & boatName & "\Bookings\FY 2023\FY 2023 " & boatName & " Charters.xlsm"
+        fullPath = basePath & "\" & boatName & "\Bookings\FY " & fiscalYear & "\FY " & fiscalYear & " " & boatName & " Charters.xlsm"
     
         Set invoiceWs = Workbooks.Open(fullPath)
         
@@ -43,33 +47,33 @@ Sub RC_GoTo_Invoicing()
             End If
         End If
         
-    ElseIf ActiveSheet.name = "RevByBoatByDay" Then
-        boatName = ActiveSheet.Cells(1, ActiveCell.Column)
-        fullName = ActiveCell.Value
-        nameArr = Split(fullName, " ")
+    'ElseIf ActiveSheet.name = "RevByBoatByDay" Then
+     '   boatName = ActiveSheet.Cells(1, ActiveCell.Column)
+      '  fullName = ActiveCell.Value
+       ' nameArr = Split(fullName, " ")
         
-        basePath = Environ("USERPROFILE") & "\Documents\SLTC\9 SLTC - Documents\Seabbatical\Fleet\" 'this should change based on computer/file setup (the first three directories)
+        'basePath = Environ("USERPROFILE") & "\Documents\SLTC\9 SLTC - Documents\Seabbatical\Fleet\" 'this should change based on computer/file setup (the first three directories)
     
-        If UBound(nameArr) >= 0 Then 'this is always true unless we made a mistake
-            lastName = nameArr(UBound(nameArr))
-        End If
+        'If UBound(nameArr) >= 0 Then 'this is always true unless we made a mistake
+            'lastName = nameArr(UBound(nameArr))
+        'End If
         
-        fullPath = basePath & boatName & "\Bookings\FY 2023\FY 2023 " & boatName & " Charters.xlsm"
+        'fullPath = basePath & boatName & "\Bookings\FY " & fiscalYear & "\FY " & fiscalYear & " " & boatName & " Charters.xlsm"
 
-        Set invoiceWs = Workbooks.Open(fullPath)
+        'Set invoiceWs = Workbooks.Open(fullPath)
         
-        If invoiceWs Is Nothing Then
-            MsgBox "Invoicing workbook not found for " & boatName & "."
-        Else
-            On Error GoTo SheetError
-            Set ws = invoiceWs.Sheets(lastName)
-            If ws Is Nothing Then
-                MsgBox "Specified sheet (" & lastName & ") was not found."
-                invoiceWs.Activate
-            Else
-                invoiceWs.Sheets(lastName).Activate
-            End If
-        End If
+        'If invoiceWs Is Nothing Then
+            'MsgBox "Invoicing workbook not found for " & boatName & "."
+        'Else
+            'On Error GoTo SheetError
+            'Set ws = invoiceWs.Sheets(lastName)
+            'If ws Is Nothing Then
+                'MsgBox "Specified sheet (" & lastName & ") was not found."
+                'invoiceWs.Activate
+            'Else
+                'invoiceWs.Sheets(lastName).Activate
+            'End If
+        'End If
     End If
     
     Exit Sub
